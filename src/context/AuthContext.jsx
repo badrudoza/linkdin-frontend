5e5da19d3c3bcf6d683b92479c53fbf07a714f43
bda <badrudozaansari0786@gmail.com>
2026-05-17 11:13:02 +0530

    first commit

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,16 @@
+import React, { createContext } from 'react'
+export const authDataContext=createContext()
+
+function AuthContext({children}) {
+const serverUrl=import.meta.env.VITE_API_URL;
+    let value={
+        serverUrl
+    }
+  return (
+     <authDataContext.Provider value={value}> 
+     {children}
+     </authDataContext.Provider> 
+  )
+}
+
+export default AuthContext
